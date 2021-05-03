c83b917b0166c0acf520f63201e05765e296b8ba
Masalha-96 <Smasalha96@gmail.com>
2021-05-03 22:16:01 +0300

    add class07 branch

--- app.js
+++ app.js
@@ -1,294 +1,113 @@
-'use strict';
+"use strict";
+let time = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
 
 function randomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function fisrtCol ()
+{
+  let tr = document.createElement("tr");
+  table.appendChild(tr);
+  let th = document.createElement("th");
+  tr.appendChild(th);
+  th.textContent = "City Name / Hour";
+
+  for (let i = 0; i < time.length; i++) 
+  {
+    let th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = `${time[i]}`;
+  }
+
+  th = document.createElement("th");
+  tr.appendChild(th);
+  th.textContent = "Daily Location Total";
+}
+
+/*
+<table> <tr> </tr> </table>
+*/
+let myDiv = document.getElementById("Sales");
+let table = document.createElement("table");
+myDiv.appendChild(table);
+
+function PatStors(cityName, minCust, maxCust, avgCookie) {
+
+
+  this.numOfCust = 0;
+
+  this.cityName = cityName;
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookie = avgCookie;
+
+  // methods
+  this.getRandomCust = function (min, max) {
+    this.numOfCust = randomNum(minCust, maxCust);
+  };
+
+  this.getRandomCust = function () {
+    randomNum(minCust, maxCust);
+  };
+
+
+
+  // addToTable function
+  this.addToTable = function () {
+    let total = 0;
+    let tr = document.createElement("tr");
+    table.appendChild(tr);
+    let th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = `${this.cityName}`;
+
+    for (let i = 0; i < time.length; i++) {
+      let td = document.createElement("td");
+      tr.appendChild(td);
+      let temp = Math.floor(randomNum(this.minCust, this.maxCust) * avgCookie);
+      td.textContent = `${temp}`;
+      total += temp;
+    }
+
+    th = document.createElement("th");
+    tr.appendChild(th);
+    th.textContent = `${total}`;
+  };
+
 }
 
 
 
-/* 1 */ let theParent = document.getElementById('Sales');
-
-let Seattle = {
-    //keys
-    name: 'Seattle',
-    time: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-    minCust: 23 ,
-    maxCust: 65 ,
-    numOfCust: 0,
-    avgCookie: 6.3 ,
-
-
-    //methods
-    getRandomCust: function (min,max)
-    {
-        this.numOfCust = randomAge(minCust, maxCust);
-    },
-
-    printAndTotal: function ()
-    {
-        let total =0;
-        
-        for ( let i = 0 ; i< this.time.length ; i++ )
-        {
-            //document.write(`${this.time[i]}: ${randomNum(23, 65)} cookies`);  
-            let listItems = document.createElement('li');
-            theParent.appendChild(listItems);
-            let temp = Math.floor(randomNum(this.minCust, this.maxCust)*Seattle.avgCookie);
-            listItems.textContent = `${this.time[i]}: ${temp} cookies`;
-            total += temp;
-        }
-
-        let listItems = document.createElement('li');
-        theParent.appendChild(listItems);
-        listItems.textContent =  `Total: ${total} cookies`;
-    
-    },
-
-
-};
-
-
-
-
-/* 2 */
-let header2 = document.createElement('h2');
-theParent.appendChild(header2);
-header2.textContent = Seattle.name;
-
-
-/*  3  */
-let unOrederedList = document.createElement('ul');
-theParent.appendChild(unOrederedList);
-unOrederedList.textContent = Seattle.printAndTotal();
-
-
+// -----------------------------------------------
+let seattle = new PatStors("Seattle", 23, 65, 6.3); // creat objects from constructor
+let tokyo = new PatStors("Tokyo", 3, 24, 1.2); 
+let paris = new PatStors("Paris", 20, 38, 2.3);
+let lima = new PatStors("Lima", 2, 16, 4.6);
+let dubai = new PatStors("Dubai", 11, 38, 3.7);
 // -----------------------------------------------
 
-
-
-let Tokyo = {
-    //keys
-    name: 'Tokyo',
-    time: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-    minCust: 3 ,
-    maxCust: 24 ,
-    numOfCust: 0,
-    avgCookie: 1.2 ,
-
-
-    //methods
-    getRandomCust: function (min,max)
-    {
-        this.numOfCust = randomAge(minCust, maxCust);
-    },
-
-    printAndTotal: function ()
-    {
-        let total =0;
-        
-        for ( let i = 0 ; i< this.time.length ; i++ )
-        {
-            //document.write(`${this.time[i]}: ${randomNum(23, 65)} cookies`);  
-            let listItems = document.createElement('li');
-            theParent.appendChild(listItems);
-            let temp = Math.floor(randomNum(this.minCust, this.maxCust)*Tokyo.avgCookie);
-            listItems.textContent = `${this.time[i]}: ${temp} cookies`;
-            total += temp;
-        }
-
-        let listItems = document.createElement('li');
-        theParent.appendChild(listItems);
-        listItems.textContent =  `Total: ${total} cookies`;
-    
-    },
-
-
-};
-
-
-
-
-/* 2 */
-header2 = document.createElement('h2');
-theParent.appendChild(header2);
-header2.textContent = Tokyo.name;
-
-
-/*  3  */
-unOrederedList = document.createElement('ul');
-theParent.appendChild(unOrederedList);
-unOrederedList.textContent = Tokyo.printAndTotal();
-
-
-// -----------------------------------------------
-
-
-let Dubai	 = {
-    //keys
-    name: 'Dubai	',
-    time: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-    minCust: 11 ,
-    maxCust: 38 ,
-    numOfCust: 0,
-    avgCookie: 3.7 ,
-
-
-    //methods
-    getRandomCust: function (min,max)
-    {
-        this.numOfCust = randomAge(minCust, maxCust);
-    },
-
-    printAndTotal: function ()
-    {
-        let total =0;
-        
-        for ( let i = 0 ; i< this.time.length ; i++ )
-        {
-            //document.write(`${this.time[i]}: ${randomNum(23, 65)} cookies`);  
-            let listItems = document.createElement('li');
-            theParent.appendChild(listItems);
-            let temp = Math.floor(randomNum(this.minCust, this.maxCust)*Dubai.avgCookie);
-            listItems.textContent = `${this.time[i]}: ${temp} cookies`; 
-            total += temp;
-        }
-
-        let listItems = document.createElement('li');
-        theParent.appendChild(listItems);
-        listItems.textContent =  `Total: ${total} cookies`;
-    
-    },
-
-
-};
-
-
-
-
-/* 2 */
-header2 = document.createElement('h2');
-theParent.appendChild(header2);
-header2.textContent = Dubai.name;
-
-
-/*  3  */
-unOrederedList = document.createElement('ul');
-theParent.appendChild(unOrederedList);
-unOrederedList.textContent = Dubai.printAndTotal();
-
-
-// -----------------------------------------------
-
-let Paris	 = {
-    //keys
-    name: 'Paris	',
-    time: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-    minCust: 20 ,
-    maxCust: 38 ,
-    numOfCust: 0,
-    avgCookie: 2.3 ,
-
-
-    //methods
-    getRandomCust: function (min,max)
-    {
-        this.numOfCust = randomAge(minCust, maxCust);
-    },
-
-    printAndTotal: function ()
-    {
-        let total =0;
-        
-        for ( let i = 0 ; i< this.time.length ; i++ )
-        {
-            //document.write(`${this.time[i]}: ${randomNum(23, 65)} cookies`);  
-            let listItems = document.createElement('li');
-            theParent.appendChild(listItems);
-           // let temp = randomNum(this.minCust, this.maxCust); // wrong one 
-           let temp = Math.floor(randomNum(this.minCust, this.maxCust)*Paris.avgCookie);
-            listItems.textContent = `${this.time[i]}: ${temp} cookies`;
-            total += temp;
-        }
-
-        let listItems = document.createElement('li');
-        theParent.appendChild(listItems);
-        listItems.textContent =  `Total: ${total} cookies`;
-    
-    },
-
-
-};
-
-
-
-
-/* 2 */
-header2 = document.createElement('h2');
-theParent.appendChild(header2);
-header2.textContent = Paris.name;
-
-
-/*  3  */
-unOrederedList = document.createElement('ul');
-theParent.appendChild(unOrederedList);
-unOrederedList.textContent = Paris.printAndTotal();
-
-
-// -----------------------------------------------
-
-
-let Lima	 = {
-    //keys
-    name: 'Lima	',
-    time: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-    minCust: 20 ,
-    maxCust: 38 ,
-    numOfCust: 0,
-    avgCookie: 2.3 ,
-
-
-    //methods
-    getRandomCust: function (min,max)
-    {
-        this.numOfCust = randomAge(minCust, maxCust);
-    },
-
-    printAndTotal: function ()
-    {
-        let total =0;
-        
-        for ( let i = 0 ; i< this.time.length ; i++ )
-        {
-            //document.write(`${this.time[i]}: ${randomNum(23, 65)} cookies`);  
-            let listItems = document.createElement('li');
-            theParent.appendChild(listItems);
-            let temp = Math.floor(randomNum(this.minCust, this.maxCust)*Lima.avgCookie);
-            listItems.textContent = `${this.time[i]}: ${temp} cookies`;
-            total += temp;
-        }
-
-        let listItems = document.createElement('li');
-        theParent.appendChild(listItems);
-        listItems.textContent =  `Total: ${total} cookies`;
-    
-    },
-
-
-};
-
-
-
-
-/* 2 */
-header2 = document.createElement('h2');
-theParent.appendChild(header2);
-header2.textContent = Lima.name;
-
-
-/*  3  */
-unOrederedList = document.createElement('ul');
-theParent.appendChild(unOrederedList);
-unOrederedList.textContent = Lima.printAndTotal();
-
-
-// -----------------------------------------------
+// call the print method to show my elemnts in the html page----
+//seattle.colOne(); 
+fisrtCol();
+seattle.addToTable();                          
+tokyo.addToTable();
+paris.addToTable();
+dubai.addToTable();
+lima.addToTable();
+// --------------------------------------------------------------
